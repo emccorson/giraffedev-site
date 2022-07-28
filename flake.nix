acb7f04be886353bc62600a80ed51ffabaf27a51
Eric Corson <public@giraffedev.com>
2022-07-28 09:20:46 +0100

    use nixos-22.05 as nixpkgs url
    My thinking is that if nixpkgs in this project matches the version of
    nixpkgs my NixOS install is using, nix might be able to use more
    packages that I already have cached. But I don't know if this is
    actually true.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "A very basic flake";
 
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
